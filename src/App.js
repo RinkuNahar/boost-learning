f88a9388bae7f43fb2fa43c4a65d0d79881acc12
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-04-17 02:21:25 +0600

    extra section reviews added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Banner from './components/Home/Banner/Banner';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Services from './components/Home/Services/Services';
 import Home from './components/Home/Home/Home';
 
 function App() {
@@ -13,6 +12,7 @@ function App() {
       <Routes>
        <Route path='/' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
