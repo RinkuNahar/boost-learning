3b1e93370fc686aa3bcdefde4d203ad1a2ed3781
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-04-18 01:42:51 +0600

    checkout added

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import checkout from '../../images/checkout.png';
+import './CheckOut.css'
 
 const CheckOut = () => {
     return (
-        <div>
-            <h2>Please CheckOut</h2>
+        <div className='checkout'>
+            <h1 className='service-title mt-4 mb-4'> Happy Learning</h1>
+            <img className='w-75 checkout-img' src={checkout} alt="" />
         </div>
     );
 };
